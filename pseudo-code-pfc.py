13c175f5eb53bf4001b34c64b1f7bd56188d7dc5
KyllianT <117281228+KyllianT@users.noreply.github.com>
2022-11-10 14:57:40 +0100

    Add files via upload

---
+++ pseudo-code-pfc.py
@@ -0,0 +1,83 @@
+#On admet une fonction random tel que on obtiens un nombre aléatoire
+
+#On admet une fonction input tels que elle renvois les choix de l'utilisateur et de l'ordinateur 
+#Definir le fonction choix_utilisateur
+ 
+ #Assigner à joueur une valeur entre "PIERRE", "FEUILLE" ou "CISEAUX"
+
+    #Tant que joueur n'a pas de valeur entre "PIERRE", "FEUILLE" ou "CISEAUX"
+    #Alors
+        #Assigner une valeur dans input avec comme parametre "PIERRE", "FEUILLE" ou "CISEAUX"
+
+    #Retourner joueur
+
+ 
+#Definir le fonction choix_ordinateur
+
+ 
+ #Assigner à n un nombre au hassard entre 1 et 3 qui definira ce que l'ordinateur ("ordi") choisit
+
+    #Si n est egale a 1
+    #Alors
+        #Assigner pierre a "ordi"
+
+    #Sinon si n est egale a 2
+    #Alors
+        #Assigner feuille a "ordi"
+
+    #Sinon n est egale a 3
+    #Alors
+        #Assigner ciseaux a "ordi"
+
+
+#Retourner la fonction "ordi"
+
+
+#Définir une valeur a et b en utilisant les 2 variables choix_utilisateur et choix_ordinateur
+
+
+#Afficher b
+
+#Si joueur est egale a "PIERRE" et que l'ordinateur est egale a "CISEAUX"
+    #Alors
+    #Afficher comme resultat: Vous avez gagne
+
+
+#Sinon si joueur est egale a "PIERRE" et que l'ordinateur est egale a "FEUILLE"
+    #Alors
+    #Afficher comme resultat: Vous avez perdu
+
+
+#Sinon si joueur est egale a "PIERRE" et que l'ordinateur est egale a "PIERRE"
+    #Alors
+    #Afficher comme resultat: Egalite
+
+ 
+#Si joueur est egale a "FEUILLE" et que l'ordinateur est egale a "PIERRE"
+    #Alors
+    #Afficher comme resultat: Vous avez gagne
+
+
+#Sinon si joueur est egale a "FEUILLE" et que l'ordinateur est egale a "CISEAUX"
+    #Alors
+    #Afficher comme resultat: Vous avez perdu
+
+
+#Sinon si joueur est egale a "FEUILLE" et que l'ordinateur est egale a "FEUILLE"
+    #Alors
+    #Afficher comme resultat:  Egalite
+
+ 
+#Sinon si joueur est egale a "CISEAUX" et que l'ordinateur est egale a "FEUILLE"
+    #Alors
+    #Afficher comme resultat: Vous avez gagne
+
+
+#Sinon si joueur est egale a "CISEAUX" et que l'ordinateur est egale a "PIERRE"
+    #Alors
+    #Afficher comme resultat: Vous avez perdu
+
+
+#Sinon si joueur est egale a "CISEAUX" et que l'ordinateur est egale a "CISEAUX"
+    #Alors
+    #Afficher comme resultat: Egalite
