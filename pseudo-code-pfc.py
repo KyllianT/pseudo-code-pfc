578c595e654e293b410760bed91ed8c987913754
KyllianT <117281228+KyllianT@users.noreply.github.com>
2022-11-10 14:33:35 +0100

    Add files via upload

---
+++ pseudo-code-pfc.py
@@ -0,0 +1,83 @@
+#On admet une fonction random tel que on obtiens un nombre aléatoire
+
+#On admet une fonction input tels que elle renvois les choix de l'utilisateur et de l'ordianrteur 
+#Definir le fonction choix_utilisateur
+ 
+ #Assigner à joueur une valeur entre pierre,feuille et ciseaux
+
+    #Tant que joueur n'a pas de valeur entre pierre feuille ou ciseau
+
+        #Reassigner à joueur une valeur entre pierre feuille ou ciseau
+
+    #Retourner joueur
+
+ 
+#Definir le fonction choix_ordinateur
+
+ 
+ #Assigner à n un nombre au hassard entre 1 et 3 qui definira ce que l'ordinateur (ordi) choisit
+
+    #Si n est egale a 1
+
+        #Assigner pierre a ordi
+
+    #Sinon si n est egale a 2
+
+        #Assigner feuille a ordi
+
+    #Sinon n est egale a 3
+
+        #Assigner ciseaux a ordi
+
+
+#Retourner la fonction ordi
+
+
+#Définir une valeur a et b en utilisant les 2 variables choix_utilisateur et choix_ordinateur
+
+
+#Afficher b
+
+#Si le joueur choisit l'option PIERRE et que l'ordinateur a choisit l'option CISEAUX
+
+    #Afficher comme resultat: Vous avez gagne
+
+
+#Sinon si le joueur choisit l'option PIERRE et que l'ordinateur a choisit l'option FEUILLE
+
+    #Afficher comme resultat: Vous avez perdu
+
+
+#Sinon si le joueur choisit l'option PIERRE et que l'ordinateur a choisit l'option PIERRE
+
+    #Afficher comme resultat: Egalite
+
+ 
+#Si le joueur choisit l'option FEUILLE et que l'ordinateur a choisit l'option PIERRE
+
+    #Afficher comme resultat: Vous avez gagne
+
+
+#Sinon si le joueur choisit l'option FEUILLE et que l'ordinateur a choisit l'option CISEAUX
+
+    #Afficher comme resultat: Vous avez perdu
+
+
+#Sinon si le joueur choisit l'option FEUILLE et que l'ordinateur a choisit l'option FEUILLE
+
+    #Afficher comme resultat:  Egalite
+
+ 
+#Sinon si le joueur choisit l'option CISEAUX et que l'ordinateur a choisit l'option FEUILLE
+
+    #Afficher comme resultat: Vous avez gagne
+
+
+#Sinon si le joueur choisit l'option CISEAUX et que l'ordinateur a choisit l'option PIERRE
+
+    #Afficher comme resultat: Vous avez perdu
+
+
+#Sinon si le joueur choisit l'option CISEAUX et que l'ordinateur a choisit l'option CISEAUX
+
+    #Afficher comme resultat: Egalite
